26a5a16930778b93d5d5d8b3694f91b0e5d9a31c
Ellen Waddle <elwa8931@colorado.edu>
2019-09-25 14:37:03 -0600

    added sys.stdin

--- get_data.py
+++ get_data.py
@@ -1,3 +1,11 @@
+#should take a column number and return an array of numbers that correspond
+#to values from stdin at the col number position
+import sys
 
 def read_stdin_col(col_num):
-    return None
+    if col_num < 1:
+        return None
+    for line in sys.stdin:
+        line=line.rstrip().split()
+
+    return line
