632089bfc383bf9a9df1fa9e1adfda167ef9a95c
Ellen Waddle <elwa8931@colorado.edu>
2019-09-26 12:36:23 -0600

    added histogram creation function

--- data_viz.py
+++ data_viz.py
@@ -3,12 +3,36 @@
 #create plots saved at output file location with mean, sd in the title and axis labeled
 import sys
 #import get_data
-#import math_lib as ml
+import math_lib as ml
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import pathlib
 
+
+def histogram(L, out_file_name):
+    file=pathlib.Path(out_file_name)
+
+    if file.exists():
+        return ('this file name is taken, choose another.')
+    else:
+        width=3
+        height=3
+        mean=ml.list_mean(L)
+        stdev=ml.list_stdev(L)
+        title='mean='+str(mean)+' stdev='+str(stdev)
+
+        fig=plt.figure(figsize=(width,height),dpi=300)
+
+        ax=fig.add_subplot(1,1,1)
+        ax.set_title(title)
+
+        ax.hist(L,3)
+
+        plt.savefig(out_file_name,bbox_inches='tight')
+
+#L=[1,2,2,3,4,5,3]
+#histogram(L,'histogram_check.png')
 
 def boxplot(L, out_file_name):
     file=pathlib.Path(out_file_name)
@@ -16,10 +40,9 @@ def boxplot(L, out_file_name):
     if file.exists():
         return ('this file name is taken, choose another.')
     else:
-        return ('file does not exist yet')
+        
 
-def histogram(L, out_file_name):
-    pass
 
 def combo(L, out_file_name):
-    pass
+    boxplot(L,out_file_name)
+    histogram(L,out_file_name)
