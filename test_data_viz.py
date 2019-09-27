a80e57dbfab06a62c7c7aceb9f7c95096a08e243
Ellen Waddle <elwa8931@colorado.edu>
2019-09-26 18:49:40 -0600

    removed second test, since i moved the check for file creation to the data_viz.py file instead

--- test_data_viz.py
+++ test_data_viz.py
@@ -8,8 +8,5 @@ class TestDataViz(unittest.TestCase):
         r=dv.boxplot([1,2,3,4],'empty.txt')
         self.assertEqual(r,'this file name is taken, choose another.')
 
-    def test_data_viz_histogram_plotname_exists_after_running(self):
-        r=dv.histogram([1,2,3,5,6,2,1]
-
 if __name__=='__main__':
     unittest.main()
